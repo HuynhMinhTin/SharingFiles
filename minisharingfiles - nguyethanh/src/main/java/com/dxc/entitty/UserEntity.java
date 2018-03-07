463b280b5cca8c86080221383218c73c654b4fb2
nguyethanh <nguyethanh@github.com>
2018-03-07 17:22:26 -0500

    project with demo download

---
+++ minisharingfiles - nguyethanh/src/main/java/com/dxc/entitty/UserEntity.java
@@ -0,0 +1,107 @@
+package com.dxc.entitty;
+
+import java.util.Set;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+
+
+@Entity(name="user")
+public class UserEntity {
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	int idUser;
+	
+	String nameUser;
+	String emailUser;
+	String passwordUser;
+	
+	@OneToOne
+	@JoinColumn(name="idPosition")
+	PositionEntity idPosition;
+	
+	@OneToMany
+	@JoinColumn(name="idFile")
+	Set<FileEntity> idFile;
+	
+	@OneToOne
+	@JoinColumn(name="idLevel")
+	LevelUserEntity idLevel;
+	
+	
+	
+	
+	
+	public UserEntity(int idUser, String nameUser, String emailUser,
+			String passwordUser, PositionEntity idPosition,
+			Set<FileEntity> idFile, LevelUserEntity idLevel) {
+		super();
+		this.idUser = idUser;
+		this.nameUser = nameUser;
+		this.emailUser = emailUser;
+		this.passwordUser = passwordUser;
+		this.idPosition = idPosition;
+		this.idFile = idFile;
+		this.idLevel = idLevel;
+	}
+	
+	public LevelUserEntity getIdLevel() {
+		return idLevel;
+	}
+	public void setIdLevel(LevelUserEntity idLevel) {
+		this.idLevel = idLevel;
+	}
+	public Set<FileEntity> getIdFile() {
+		return idFile;
+	}
+	public void setIdFile(Set<FileEntity> idFile) {
+		this.idFile = idFile;
+	}
+	public PositionEntity getIdPosition() {
+		return idPosition;
+	}
+	public void setIdPosition(PositionEntity idPosition) {
+		this.idPosition = idPosition;
+	}
+	public int getIdUser() {
+		return idUser;
+	}
+	public void setIdUser(int idUser) {
+		this.idUser = idUser;
+	}
+	public String getNameUser() {
+		return nameUser;
+	}
+	public void setNameUser(String nameUser) {
+		this.nameUser = nameUser;
+	}
+	public String getEmailUser() {
+		return emailUser;
+	}
+	public void setEmailUser(String emailUser) {
+		this.emailUser = emailUser;
+	}
+	public String getPasswordUser() {
+		return passwordUser;
+	}
+	public void setPasswordUser(String passwordUser) {
+		this.passwordUser = passwordUser;
+	}
+	public UserEntity() {
+		super();
+	}
+	@Override
+	public String toString() {
+		return "UserEntity [idUser=" + idUser + ", nameUser=" + nameUser + ", emailUser=" + emailUser
+				+ ", passwordUser=" + passwordUser + ", idFile=" + idFile + ", idPosition=" + idPosition + "]";
+	}
+	
+	
+	
+	
+}
