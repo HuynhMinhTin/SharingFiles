aa98be2b1f80d33dabaaa6d3f3f6950eb8b57149
MinhTin <tin19962014@gmail.com>
2018-03-13 08:50:10 -0400

    update for search

---
+++ minisharingfiles/src/main/java/com/dxc/service/SearchService.java
@@ -0,0 +1,33 @@
+package com.dxc.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.dxc.dao.SearchDAO;
+import com.dxc.daoImp.SearchImp;
+import com.dxc.entitty.FileEntity;
+
+
+@Service
+public class SearchService implements SearchImp{
+	
+	@Autowired
+	SearchDAO searchDAO;
+	
+
+	public List<FileEntity> SearchFile(String keyword) {
+		// TODO Auto-generated method stub
+		
+		List<FileEntity> file = searchDAO.SearchFile(keyword);
+		
+		return file;
+	}
+
+	public void indexBooks() throws Exception {
+		// TODO Auto-generated method stub
+		searchDAO.indexBooks();
+	}
+
+}
