8f414edd1ddfaab401bae2e8541b41aa7fce91e8
MinhTin <tin19962014@gmail.com>
2018-03-14 17:53:18 -0400

    Signed-off-by: MinhTin <tin19962014@gmail.com>

---
+++ minisharingfiles/src/main/java/com/dxc/controller/ApiController.java
@@ -0,0 +1,27 @@
+package com.dxc.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.SessionAttributes;
+
+import com.dxc.service.FileService;
+
+
+@Controller
+@RequestMapping("/home")
+public class ApiController {
+	
+	@Autowired
+	FileService fileService;
+	
+	
+	
+}
